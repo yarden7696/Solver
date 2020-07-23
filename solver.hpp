ff148ea6daa9325c414a52f46eccff9e3f489c5f
Yarden Cohen <57362284+yarden7696@users.noreply.github.com>
2020-07-23 13:52:13 +0300

    Update solver.hpp

--- solver.hpp
+++ solver.hpp
@@ -5,6 +5,7 @@ using namespace std;
 namespace solver{
     
 class RealVariable{
+    
     private:
     double a,b,c;
 
@@ -12,15 +13,9 @@ class RealVariable{
     RealVariable(double a ,double b, double c);
     RealVariable(): a(0), b(1), c(0) {}
 
-    const double & getA () const{
-        return this->a;
-    }
-    const double & getB () const{
-        return b;
-    }
-    const double & getC () const{
-        return c;
-    }
+    const double & getA () const { return this->a; }
+    const double & getB () const { return b; }
+    const double & getC () const { return c; }
 
     // + operator
     friend RealVariable operator+(const RealVariable& x, const RealVariable &y );
@@ -49,18 +44,17 @@ class RealVariable{
     friend RealVariable operator==(const RealVariable& x, const RealVariable& y);
     friend RealVariable operator==(const RealVariable& X, const double y);
     friend RealVariable operator==(const double y, const RealVariable& X);
-
-    //bool operator! () const;
     };
 
     class ComplexVariable{
+        
     private:
         complex <double> a;
         complex <double> b;
         complex <double> c;
 
     public:
-        ComplexVariable(): a(0), b(1,0), c(0) {}
+        ComplexVariable(): a(0), b(1,0), c(0) {} // its mean : z^2(0+0i)+z(1+0i)+0
 
         ComplexVariable(complex <double> a,complex <double> b,complex <double> c){
             this->a = complex<double>(a);
@@ -72,29 +66,28 @@ class RealVariable{
         const complex<double> &getB () const { return b; }
         const complex<double> &getC () const { return c; }
 
-//        //setters
-//        void setA (&complex<double> a) const {this->a=a ; }
-//        void setB (&complex<double> b) const {this->b=b ; }
-//        void setC (&complex<double> c) const {this->c=c ; }
-
         // + operator
         friend ComplexVariable operator+(const ComplexVariable& x,const ComplexVariable &y );
         friend ComplexVariable operator+(const ComplexVariable& x, const complex<double> y );
         friend ComplexVariable operator+(const complex<double> y,const ComplexVariable& x);
+        
         // - operator
         friend ComplexVariable operator-(const ComplexVariable& x,const ComplexVariable &y );
         friend ComplexVariable operator-(const ComplexVariable& x,const complex<double> y);
         friend ComplexVariable operator-(const complex<double> y,const ComplexVariable& x);
+        
         //* operator
         friend ComplexVariable operator*(const ComplexVariable& x, const ComplexVariable &y);
         friend ComplexVariable operator*(const ComplexVariable& x, const complex<double> y);
         friend ComplexVariable operator*(const complex<double> y,const ComplexVariable& x);
+        
         //: operator
         friend ComplexVariable operator/(const ComplexVariable& x, const ComplexVariable &y);
         friend ComplexVariable operator/(const ComplexVariable& x, const complex<double> y);
-        // friend ComplexVariable operator/(const complex<double> y,const ComplexVariable& x);check this
+        
         // ^ operator
         friend ComplexVariable operator^(const ComplexVariable &x, const complex<double> power);
+        
         // == operator
         friend ComplexVariable operator==(const ComplexVariable &x, const ComplexVariable &y);
         friend ComplexVariable operator==(const ComplexVariable &x, const complex<double> y);
@@ -103,7 +96,6 @@ class RealVariable{
     };
 
     double solve (const RealVariable& x);
-    //double solve(const bool equation);
     complex<double> solve (const ComplexVariable & x);
 
 }
